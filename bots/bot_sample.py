c7ac803a1990e1c576fcfcd93d237143d03cb17c
Tawan Thampipattanakul <tawan.tpptnk@gmail.com>
2017-07-20 20:39:51 +0700

    UPDATE bot sample

--- bots/bot_sample.py
+++ bots/bot_sample.py
@@ -9,13 +9,21 @@ class BotSample ( TankPrototype ):
     '''
     getHP()                get current HP
     getMP()                get current MP
+    getName()              get name
+    getPosition()            get x, y position
+    isMoving()               check if moving( cant move while moving )
+    getTankInfoList()       get a list of tank information
+    isAtEdge(direction)       check if at edge of bettle field
+    getDirection()           get heading direction ( also moving directiohn if is moving )
     isAlly(another_bot)    check if another_bot is an alliance or not
     getTeamColor()         get current team color
     readyToMove()          check if ready to move ( cooldown and mana cost )
     readyToShoot()         check if ready to shoot ( cooldown and mana cose )
+    move(direction)
     shoot(direction)       fire a bullet in the specified direction ('left' 'right' 'up' 'down') : cost ?? mana
     heal(direction)        fire a potion in the specified direction ('left' 'right' 'up' 'down')
     '''
+    
     def start( self ) :
         #this method will be call at the beginning once
         #use this to initialize variable you will use in your tank algorithm
