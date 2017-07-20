225542e9eb3542b2ddfaed8affc344a2ea215879
Panupong <most54321@gmail.com>
2017-07-20 21:54:10 +0700

    Added win screens

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ class Game:
         pygame.mixer.init()
 
         self.background = pygame.image.load("source/bg.png")
+        self.red_won = pygame.image.load("source/red_won.png")
+        self.green_won = pygame.image.load("source/green_won.png")
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode([650, 650])
         
@@ -53,18 +55,35 @@ class Game:
         
     def getTankInfoList(self):
         return self.__tank_info_list[:]
+
+    def lost(self, team_color):
+        for tank in self.tank_list:
+            if tank.getTeamColor() == team_color:
+                return False
+        return True
     
     def main(self):
+        winner = ""
         while True:
+            ##Drawings
             self.screen.blit(self.background, [0, 0])
-            self.clearDestroyedTankInfo()
-            [ func() for func in self.__hidden_update_list ]
-            self.sprite_list.update()
             self.sprite_list.draw(self.screen)
             for tank in self.tank_list:
                 tank.drawBars(self.screen)
+            if winner == "green":
+                self.screen.blit(self.green_won, [0, (SCREEN_WIDTH - BLOCK_SIZE)/2])
+            elif winner == "red":
+                self.screen.blit(self.red_won, [0, (SCREEN_WIDTH - BLOCK_SIZE)/2])
             pygame.display.update()
-            
+
+            ##Logics
+            if self.lost("red"):
+                winner = "green"
+            elif self.lost("green"):
+                winner = "red"
+            [ func() for func in self.__hidden_update_list ]
+            self.clearDestroyedTankInfo()
+            self.sprite_list.update()
             self.processEvents()
             self.clock.tick(FPS)
 
